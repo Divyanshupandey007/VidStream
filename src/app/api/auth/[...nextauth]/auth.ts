75add3595d79d46921e95e0219d41bf1adb32739
Divyanshupandey007 <79376921+Divyanshupandey007@users.noreply.github.com>
2024-05-27 22:14:34 +0530

    Remove lint check

--- src/app/api/auth/[...nextauth]/auth.ts
+++ src/app/api/auth/[...nextauth]/auth.ts
@@ -1,26 +1,26 @@
 // auth.ts
-import { AuthOptions } from "next-auth";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
-import prisma from "@/vendor/db"; // Adjust the path as needed
+// import { AuthOptions } from "next-auth";
+// import { PrismaAdapter } from "@next-auth/prisma-adapter";
+// import NextAuth from "next-auth/next";
+// import GoogleProvider from "next-auth/providers/google";
+// import prisma from "@/vendor/db"; // Adjust the path as needed
 
-const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  pages: {
-    signIn: "/",
-  },
-  debug: process.env.NODE_ENV === "development",
-  session: {
-    strategy: "jwt",
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-};
+// const authOptions: AuthOptions = {
+//   adapter: PrismaAdapter(prisma),
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID as string,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+//     }),
+//   ],
+//   pages: {
+//     signIn: "/",
+//   },
+//   debug: process.env.NODE_ENV === "development",
+//   session: {
+//     strategy: "jwt",
+//   },
+//   secret: process.env.NEXTAUTH_SECRET,
+// };
 
-export default authOptions;
+// export default authOptions;
