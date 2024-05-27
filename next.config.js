bdb5a4b01620a58e78f025697b09ea39a58fe76a
Divyanshupandey007 <79376921+Divyanshupandey007@users.noreply.github.com>
2024-05-27 16:33:28 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
       domains: ["lh3.googleusercontent.com", "res.cloudinary.com"],
     },
+    eslint: {
+      ignoreDuringBuilds: false,
+    },
   };
   
   module.exports = nextConfig;
