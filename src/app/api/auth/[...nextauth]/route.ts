aeb9dcc92678bda57bf1d2de3e0abae43b1275d5
Divyanshupandey007 <79376921+Divyanshupandey007@users.noreply.github.com>
2024-06-02 19:24:48 +0530

    Instance commit

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -29,13 +29,64 @@
 
 
 
+// import prisma from "@/vendor/db";
+// import { PrismaAdapter } from "@next-auth/prisma-adapter";
+// import { NextApiHandler } from "next";
+// import NextAuth, { NextAuthOptions } from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+
+// export const authOptions: NextAuthOptions = {
+//   adapter: PrismaAdapter(prisma),
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID as string,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+//     }),
+//   ],
+//   pages: {
+//     signIn: "/",
+//   },
+//   debug: process.env.NODE_ENV === "development",
+//   session: {
+//     strategy: "jwt",
+//   },
+//   secret: process.env.NEXTAUTH_SECRET,
+// };
+
+// const handler: NextApiHandler = (req, res) => NextAuth(req, res, authOptions);
+
+// export { handler as GET, handler as POST };
+
+
+
+
+
+
+
+// route.ts
+// import authOptions from "./auth"; // Update the path as needed
+// import NextAuth from "next-auth/next";
+
+// const handler = NextAuth(authOptions);
+
+// export { handler as GET, handler as POST };
+
+
+
+
+
+
+
+
+
 import prisma from "@/vendor/db";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { NextApiHandler } from "next";
-import NextAuth, { NextAuthOptions } from "next-auth";
+import { AuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+// import NextAuth, { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions: NextAuthOptions = {
+export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
@@ -53,20 +104,9 @@ export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
 };
 
-const handler: NextApiHandler = (req, res) => NextAuth(req, res, authOptions);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
 
 
 
-
-
-
-
-// route.ts
-// import authOptions from "./auth"; // Update the path as needed
-// import NextAuth from "next-auth/next";
-
-// const handler = NextAuth(authOptions);
-
-// export { handler as GET, handler as POST };
